2200876477cc453bf37f24313992ccb64679cfb2
orenjiakira <orenjiakira@gmail.com>
2019-01-26 11:53:34 -0200

    Oh god

---
+++ src/Database/Database.gd
@@ -0,0 +1,9 @@
+extends Node
+
+func item_exists(item):
+	return has_node(item)
+
+func instance_item_icon(item):
+	assert(item_exists(item))
+	var icon_sprite = get_node(item)
+	return icon_sprite.clone()
