c22fdf8b4bd95d8a63eeab0dcac790f428812480
orenjiakira <orenjiakira@gmail.com>
2019-01-27 14:36:20 -0200

    Fix screen screamer

--- src/HomeScreen/HomeScreen.gd
+++ src/HomeScreen/HomeScreen.gd
@@ -1,56 +1,53 @@
-extends Node2D
+extends "res://Screen.gd"
 
-const DESERTIA = preload("res://Maps/Desert_Map.tscn")
-const FURRIA = preload("res://Maps/Earth_Map.tscn")
-const FREEZIA = preload("res://Maps/Ice_Map.tscn")
+const DESERTIA = "res://Maps/Desert_Map.tscn"
+const FURRIA = "res://Maps/Earth_Map.tscn"
+const FREEZIA = "res://Maps/Ice_Map.tscn"
 
+onready var title = get_node("Title")
 onready var open_animation_player = get_node("Title/AnimationPlayer")
 onready var press_start_animation_player = get_node("Label/AnimationPlayer")
 onready var screen_screamer = get_node("/root/ScreenScreamer")
-onready var text_box = get_node("/root/Hud/TextBox")
 onready var root = get_tree().get_root()
+
+var choosing_planet = false
 var planet = 0
 var planet_names =["DesertCompass","FreezeCompass","EarthCompass"] 
-
 
 func _ready():
 	for planetstring in planet_names:
 		get_node(planetstring).hide()
 	get_node("DesertCompass").show()
-	screen_screamer.set_screen("world_selection")
 	set_process_input(false)
 	yield(open_animation_player, "animation_finished")
 	press_start_animation_player.play("Auto")
 	set_process_input(true)
 
 func _input(event):
-	if event.is_action_pressed("ACTION_CONFIRM"):
-		match planet: 
-			0:
-				start_game(DESERTIA)
-			1:
-				start_game(FREEZIA)
-			2:
-				start_game(FURRIA)
-	if event.is_action_pressed("DIR_RIGHT"):
-		planet = (planet + 1) %3
-		for planetstring in planet_names:
-			get_node(planetstring).hide()
-		get_node(planet_names[planet]).show()
-	if event.is_action_pressed("DIR_LEFT"):
-		planet = (planet - 1) %3
-		for planetstring in planet_names:
-			get_node(planetstring).hide()
-		get_node(planet_names[planet]).show()
+	if choosing_planet:
+		if event.is_action_pressed("ACTION_CONFIRM"):
+				match planet: 
+					0:
+						start_game(DESERTIA)
+					1:
+						start_game(FREEZIA)
+					2:
+						start_game(FURRIA)
+		if event.is_action_pressed("DIR_RIGHT"):
+			planet = (planet + 1) %3
+			for planetstring in planet_names:
+				get_node(planetstring).hide()
+			get_node(planet_names[planet]).show()
+		if event.is_action_pressed("DIR_LEFT"):
+			planet = (planet - 1) %3
+			for planetstring in planet_names:
+				get_node(planetstring).hide()
+			get_node(planet_names[planet]).show()
+	else:
+		if event.is_action_pressed("ACTION_CONFIRM"):
+			choosing_planet = true
+			title.hide()
 
 func start_game(world):
 		set_process_input(false)
-		var loading_screen = get_node("/root/LoadingScreen")
-		loading_screen.fade_out(0.5)
-		yield(loading_screen, "faded_out")
-		root.add_child(world.instance())
-		screen_screamer.set_screen("game")
-		hide()
-		loading_screen.fade_in(0.5)
-		yield(loading_screen, "faded_in")
-		queue_free()
+		screen_screamer.set_screen(world)
--- src/Misc/ScreenScreamer.gd
+++ src/Misc/ScreenScreamer.gd
@@ -1,6 +1,24 @@
 extends Node
 
+
 signal set_screen
 
-func set_screen(screen_name):
-	emit_signal("set_screen", screen_name)
+onready var loading_screen = get_node("/root/LoadingScreen")
+
+var current_screen
+
+func register_screen(screen):
+	current_screen = screen
+
+func set_screen(screen_path):
+	loading_screen.fade_out(0.5)
+	yield(loading_screen, "faded_out")
+	if current_screen:
+		current_screen.queue_free()
+	print(screen_path)
+	var screen_scene = load(screen_path)
+	current_screen = screen_scene.instance()
+	get_node("/root").add_child(current_screen)
+	emit_signal("set_screen", screen_path)
+	loading_screen.fade_in(0.5)
+	yield(loading_screen, "faded_in")
